bbac973b0d0d99a839e9dd7aad3688ce27608569
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-25 19:15:12 +0100

    Initial file commit

---
+++ Pandas_GroupBy.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv('Universities.csv')
+
+print(df.head())
+
+#Mean number of completions per year (Year is a category, Completions is continuous)
+print(df.groupby('Year').mean('Completions'))
+print('**********************************************')
+
+#Group by multiple categories. Outer index is 'Year', inner index is 'Sector'
+print(df.groupby(['Year', 'Sector']).sum('Completions'))
+print('**********************************************')
+
+#Output aggregate statistics for completions each year
+print(df.groupby('Year').describe().transpose())
+print('**********************************************')
